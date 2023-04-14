f05db729456d94db20a67d40b70cd180ec5bfdd7
Raphael Cottier <github.zing@dfgh.net>
2023-04-14 16:30:36 +0200

    First try to handle creating Highlight on readwise from Tolino entries

--- main.go
+++ main.go
@@ -1,4 +1,54 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"time"
+
+	//"github.com/ssppooff/tolino-readwise-sync/readwise"
+	"github.com/ssppooff/tolino-readwise-sync/tolino"
+)
+
+func main() {}
+
+const appIdentifier = "tolino-sync"
+
+type HlCreate struct {
+	Text           string
+	Note           string
+	Title, Author  string
+	Location       int    // page number
+	Location_type  string // always page
+	Source_type    string // app identifier
+	Category       string // always books
+	Highlighted_at string // ISO 8601
+}
+
+// Convert converts multiple Tolino entries into a JSON string suitable for sending to API call Highlight CREATE
+//
+// Sets the 'location_type' to 'page', 'category' to 'books', and 'source_type' to the content of the constant 'appIdentifier'
+func Convert(tes []tolino.Entry) (string, error) {
+	type payload struct{ Highlights []HlCreate }
+	var p = payload{}
+
+	for _, te := range tes {
+		var tmp = HlCreate{
+			Text:           te.Highlight,
+			Note:           te.Note,
+			Title:          te.Title,
+			Author:         te.Author,
+			Location:       te.Page,
+			Location_type:  "page",
+			Source_type:    appIdentifier,
+			Category:       "books",
+			Highlighted_at: te.Date.Format(time.RFC3339),
+		}
+		p.Highlights = append(p.Highlights, tmp)
+	}
+
+	b, err := json.Marshal(p)
+	if err != nil {
+		return "", err
+	}
+
+	return string(b), nil
 }
---
+++ main_test.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	// "encoding/json"
+	// "encoding/json"
+	// "fmt"
+	"testing"
+	"time"
+
+	"github.com/ssppooff/tolino-readwise-sync/tolino"
+)
+
+func TestConvert(t *testing.T) {
+	tmpTime, _ := time.ParseInLocation(time.DateTime, "2022-09-01 21:36:00", time.Local)
+	var te1 = tolino.Entry{Title: "Random Title", Author: "Random Author", Page: 33, Note: "Some Note", Highlight: "High Text", Changed: false, Date: tmpTime}
+	var te2 = tolino.Entry{Title: "Another Random Title", Author: "Random Author", Page: 73, Note: "Some Note", Highlight: "High Text", Changed: false, Date: tmpTime.Add(time.Hour * 1)}
+
+	t.Run("single entry", func(t *testing.T) {
+		got, err := Convert([]tolino.Entry{te1})
+		want := `{"Highlights":[{"Text":"High Text","Note":"Some Note","Title":"Random Title","Author":"Random Author","Location":33,"Location_type":"page","Source_type":"tolino-sync","Category":"books","Highlighted_at":"2022-09-01T21:36:00+02:00"}]}`
+
+		if err != nil {
+			t.Fatalf("got an error, didn't want one: %v", err)
+		}
+
+		if got != want {
+			t.Errorf("\ngot : %q,\nwant: %q", got, want)
+		}
+	})
+
+	t.Run("multiple entries", func(t *testing.T) {
+		got, err := Convert([]tolino.Entry{te1, te2})
+		want := `{"Highlights":[{"Text":"High Text","Note":"Some Note","Title":"Random Title","Author":"Random Author","Location":33,"Location_type":"page","Source_type":"tolino-sync","Category":"books","Highlighted_at":"2022-09-01T21:36:00+02:00"},{"Text":"High Text","Note":"Some Note","Title":"Another Random Title","Author":"Random Author","Location":73,"Location_type":"page","Source_type":"tolino-sync","Category":"books","Highlighted_at":"2022-09-01T22:36:00+02:00"}]}`
+
+		if err != nil {
+			t.Fatalf("got an error, didn't want one: %v", err)
+		}
+
+		if got != want {
+			t.Errorf("\ngot : %q,\nwant: %q", got, want)
+		}
+	})
+}
